32f90b6478a7c377111ef2b29093e71e532417f3
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-09 22:59:40 -0300

    altera var e function

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,8 +1,8 @@
 "use strict";
-function somar(numero1, numero2) {
+function soma(numero1, numero2) {
     return numero1 + numero2;
 }
-var numero1 = 20;
-var numero2 = 5;
-var resultado = somar(numero1, numero2);
+var numero1 = 22;
+var numero2 = 50;
+var resultado = soma(numero1, numero2);
 console.log(resultado);
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,9 +1,9 @@
-function somar(numero1: number, numero2: number){
+function soma(numero1: number, numero2: number){
     return numero1 + numero2;
 }
 
-const numero1 = 20;
-const numero2 = 5;
+const numero1 = 22;
+const numero2 = 50;
 
-const resultado = somar(numero1, numero2);
+const resultado = soma(numero1, numero2);
 console.log(resultado);
