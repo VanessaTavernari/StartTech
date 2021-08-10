5461c7c04d477f71d28ca499ab777f32dbeb1146
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-09 22:11:55 -0300

    Initial commit

---
+++ TypeScript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+var numero1 = 20;
+var numero2 = 5;
+var resultado = somar(numero1, numero2);
+console.log(resultado);
---
+++ TypeScript/index.ts
@@ -0,0 +1,9 @@
+function somar(numero1: number, numero2: number){
+    return numero1 + numero2;
+}
+
+const numero1 = 20;
+const numero2 = 5;
+
+const resultado = somar(numero1, numero2);
+console.log(resultado);
