90f41d8f01aa603eb70ed564b3fb33c7fcbd6829
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-11 19:47:25 -0300

    inherit a class

--- TypeScript/classe.ts
+++ TypeScript/classe.ts
@@ -41,3 +41,53 @@ class Pessoa {
 
 const pessoa = new Pessoa("Ana", "Mendes", "Avenida Professor Dadinho, 171");
 console.log(pessoa.nome);
+
+
+// CLASSE ...
+class Carro {
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMaxima: number = 220
+    ) { }
+    private alterarVelocidade(delta: number) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+
+        if(novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+        } else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+    }
+
+    acelerar() {
+        this.alterarVelocidade(5);
+    }
+
+    frear() {
+        this.alterarVelocidade(-5);
+    }
+}
+
+const carro = new Carro("Chevrolet", "Prisma");
+carro.acelerar();
+
+// HERANÇA ...
+class Camaro extends Carro {
+    private turbo = false;
+
+    constructor() {
+        super("Chevrolet", "Camaro", 500);
+    }
+
+    ligarTurbo() {
+        this.turbo = true;
+    }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
