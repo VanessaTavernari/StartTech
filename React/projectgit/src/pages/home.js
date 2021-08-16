ab4d1867ef291989efd07c46764ef680a8710270
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-16 00:42:34 -0300

    add storage

--- React/projectgit/src/pages/home.js
+++ React/projectgit/src/pages/home.js
@@ -6,7 +6,15 @@ function App(props) {
   const [usuario, setUsuario] = useState("");
   
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    });
   }
 
   return (
