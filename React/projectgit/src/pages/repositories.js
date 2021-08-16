b37b4c877565c22c9cd2e6cb03185d43fba10af3
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-16 01:21:08 -0300

    add declarative programming

--- React/projectgit/src/pages/repositories.js
+++ React/projectgit/src/pages/repositories.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from './styled-repositories'
 
 export default function Repositories() {
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    }, []);
+
     return(
         <S.Container>
             <S.Title>Repositories</S.Title>
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+                { repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: { repository } </S.ListItem>
+                    )
+                })}
             </S.List>
         </S.Container>
     )
