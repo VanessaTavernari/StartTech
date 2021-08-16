54884315d806eba377583075c96e7ab976e422d4
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-16 01:16:49 -0300

    add repository

--- React/projectgit/src/pages/home.js
+++ React/projectgit/src/pages/home.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import * as S from './styled-home';
+import { useHistory } from 'react-router-dom';
 
 function App(props) {
+  const history = useHistory();
   const [usuario, setUsuario] = useState("");
   
   function handlePesquisa() {
@@ -14,6 +16,7 @@ function App(props) {
         repositoriesName.push(repository.name);
       });
       localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      history.push('/repositories');
     });
   }
 
