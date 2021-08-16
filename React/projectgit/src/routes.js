d8cfc2507a8ff42e8f95d48e8d6e287bd171aae4
Vanessa T Tavernari <vanessa.tavernari@gmail.com>
2021-08-15 23:14:08 -0300

    import route

--- React/projectgit/src/routes.js
+++ React/projectgit/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import Repositories from './Repositories';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Repositories from './pages/repositories';
+import Home from './pages/home';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path='/' exact component={Home} />
                 <Route path='/repositories' component={Repositories}/>
             </Switch>
         </BrowserRouter>
